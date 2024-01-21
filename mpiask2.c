8f01d0cb9a5f104450ec5afc3272bf567ac3efbc
Stelios <55328611+stafidopsomo@users.noreply.github.com>
2024-01-21 13:26:13 +0000

    Refactor matrix dominance check and add comments

--- mpiask2.c
+++ mpiask2.c
@@ -11,24 +11,24 @@ int main(int argc, char *argv[]) {
     // Αρχικοποίηση του MPI
     MPI_Init(&argc, &argv);
 
-    // Λήψη του rank και του size
+    // Λήψη του rank και του size της τρέχουσας διεργασίας
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Έλεγχος αν έχει δοθεί η παράμετρος N
+    // Έλεγχος για την ύπαρξη της παραμέτρου N
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <number>\n", argv[0]);
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    // Μετατροπή της παραμέτρου σε ακέραιο
+    // Μετατροπή της παραμέτρου N από string σε ακέραιο
     N = atoi(argv[1]); 
 
     // Δέσμευση μνήμης για τους πίνακες A και B
     A = (double *)malloc(N * N * sizeof(double));
     B = (double *)malloc(N * N * sizeof(double));
 
-    // Έλεγχος αν η δέσμευση μνήμης ήταν επιτυχής
+    // Έλεγχος για επιτυχή δέσμευση μνήμης
     if (A == NULL || B == NULL) {
         printf("Memory allocation failed for array A or B\n");
         free(A);
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Ο πίνακας A συμπληρώνεται από τον χρήστη
+    // Εισαγωγή στοιχείων στον πίνακα A από τον χρήστη (μόνο από την rank 0 διεργασία)
     if (rank == 0) {
         printf("Please enter the elements of the array\n");
         for (i = 0; i < N; i++) {
@@ -46,11 +46,10 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Εκπομπή του πίνακα A σε όλες τις διεργασίες
+    // Εκπομπή των στοιχείων του πίνακα A σε όλες τις διεργασίες
     MPI_Bcast(A, N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    // Έλεγχος αν ο πίνακας A είναι strictly dominant
-    // Κάθε διεργασία ελέγχει ένα υποσύνολο των γραμμών του πίνακα
+    // Έλεγχος αυστηρής κυριαρχίας για τον πίνακα A
     for (i = rank; i < N; i += size) {
         double rowSum = 0.0, diagVal = fabs(A[i * N + i]);
         for (j = 0; j < N; j++) {
@@ -64,42 +63,40 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Συγχρονισμός των διεργασιών και έλεγχος αν ο πίνακας είναι strictly dominant
+    // Συγχρονισμός διεργασιών και έλεγχος πίνακα για αν είναι αυστηρά διαγώνια δεσπόζων
     int globalDominant;
     MPI_Allreduce(&isStrictlyDominant, &globalDominant, 1, MPI_INT, MPI_LAND, MPI_COMM_WORLD);
 
-    // Εκτύπωση του αποτελέσματος
+    // Εκτύπωση αποτελέσματος για το αν ο πίνακας ειναι αυστηρά διαγώνια δεσπόζων
     if (rank == 0) {
         if (globalDominant) {
-            printf("Yes\n");
+            printf("Yes, the matrix is strictly dominant\n");
         } else {
-            printf("No\n");
+            printf("No, the matrix is not strictly dominant\n");
             MPI_Finalize();
             return 0;
         }
     }
 
-// Calculation of the maximum absolute value of the diagonal of matrix A
-m = fabs(A[rank * N + rank]);
-for (i = rank + size; i < N; i += size) {
-    double temp = fabs(A[i * N + i]);
-    if (temp > m) {
-        m = temp;
+    // Υπολογισμός μέγιστης απόλυτης τιμής στη διαγώνιο του πίνακα A
+    m = fabs(A[rank * N + rank]);
+    for (i = rank + size; i < N; i += size) {
+        double temp = fabs(A[i * N + i]);
+        if (temp > m) {
+            m = temp;
+        }
     }
-}
 
-// Synchronization of the processes and finding the maximum absolute value
-double globalMax;
-MPI_Allreduce(&m, &globalMax, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+    // Συγχρονισμός διεργασιών και εύρεση της μέγιστης απόλυτης τιμής
+    double globalMax;
+    MPI_Allreduce(&m, &globalMax, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
 
-if (rank == 0) {
-    printf("The max absolute value of the diagonal is %lf\n", globalMax);
-}
-                    
-                    
+    // Εκτύπωση της μέγιστης απόλυτης τιμής της διαγωνίου
+    if (rank == 0) {
+        printf("The max absolute value of the diagonal is %lf\n", globalMax);
+    }
 
-
-    // Δημιουργία του πίνακα B
+    // Δημιουργία του πίνακα B βάσει της μέγιστης τιμής
     for (i = rank; i < N; i += size) {
         for (j = 0; j < N; j++) {
             int index = i * N + j;
@@ -111,7 +108,7 @@ if (rank == 0) {
         }
     }
 
-    // Εκτύπωση του πίνακα B
+    // Εκτύπωση του πίνακα B (μόνο από την rank 0 διεργασία)
     if (rank == 0){
         printf("The new matrix Β is:\n");
         for (i = 0; i < N; i++) {
@@ -122,31 +119,31 @@ if (rank == 0) {
         }
     } 
 
-// Calculation of the minimum value of matrix B
-double localMin = B[rank * N];
-int localMinPos = rank * N;
-for (i = rank; i < N; i += size) {
-    for (j = 0; j < N; j++) {
-        int index = i * N + j;
-        if (B[index] < localMin) {
-            localMin = B[index];
-            localMinPos = index;
+    // Υπολογισμός ελάχιστης τιμής του πίνακα B
+    double localMin = B[rank * N];
+    int localMinPos = rank * N;
+    for (i = rank; i < N; i += size) {
+        for (j = 0; j < N; j++) {
+            int index = i * N + j;
+            if (B[index] < localMin) {
+                localMin = B[index];
+                localMinPos = index;
+            }
         }
     }
-}
 
-// Synchronization of the processes and finding the minimum value
-double globalMin;
-int globalMinPos;
-MPI_Allreduce(&localMin, &globalMin, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
-MPI_Allreduce(&localMinPos, &globalMinPos, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+    // Συγχρονισμός διεργασιών και εύρεση της ελάχιστης τιμής
+    double globalMin;
+    int globalMinPos;
+    MPI_Allreduce(&localMin, &globalMin, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+    MPI_Allreduce(&localMinPos, &globalMinPos, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
 
-if (rank == 0) {
-    printf("The min value of matrix B is %lf at position (%d, %d)\n", globalMin, globalMinPos / N + 1, globalMinPos % N + 1);
-}
+    // Εκτύπωση της ελάχιστης τιμής του πίνακα B
+    if (rank == 0) {
+        printf("The min value of matrix B is %lf at position (%d, %d)\n", globalMin, globalMinPos / N + 1, globalMinPos % N + 1);
+    }
 
-
-    // Απελευθέρωση της μνήμης και τερματισμός του MPI
+    // Απελευθέρωση μνήμης και τερματισμός του MPI
     free(A);
     free(B);
     MPI_Finalize();
